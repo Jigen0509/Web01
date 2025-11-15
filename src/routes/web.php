8156a1325329ef4b3988a33c691ae29a8b8758a5
Jigen0509 <509jigen@gmail.com>
2025-11-15 12:04:18 +0900

    Redirect dashboard to points page

--- src/routes/web.php
+++ src/routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/points');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
