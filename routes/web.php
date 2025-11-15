707e9ae369d6df299e5dbe4d6498f1dfad5aa3f3
Jigen0509 <509jigen@gmail.com>
2025-11-15 13:44:11 +0900

    Redirect root to login and dashboard to points

--- routes/web.php
+++ routes/web.php
@@ -20,11 +20,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return redirect('/points');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
