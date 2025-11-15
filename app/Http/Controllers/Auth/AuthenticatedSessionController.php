83f0fcdfd23a1d271afabe30051e431fe7c5d7a1
Jigen0509 <509jigen@gmail.com>
2025-11-15 13:56:13 +0900

    fix(auth): force redirect to /points after login (root app)

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,8 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        // Always send users to the points page after login
+        return redirect('/points');
     }
 
     /**
